f7242d2fad4c0f287f7de49af863b4213c571ba3
uselessface <uselessface@mail.ru>
2023-05-23 06:09:34 +0400

    hacj

--- main.js
+++ main.js
@@ -27,8 +27,8 @@ function renderApp() {
     const data = getData();
     data.then(data => {
         togglePreloader();
-        renderUsersList(data[0]);
-        renderTasksList(data[1]);
+        renderUsersList(data.userList);
+        renderTasksList(data.taskList);
     });
 }
 
@@ -36,16 +36,43 @@ function getData() {
     const userUrl = 'https://varankin_dev.elma365.ru/api/extensions/2a38760e-083a-4dd0-aebc-78b570bfd3c7/script/users';
     const taskUrl = 'https://varankin_dev.elma365.ru/api/extensions/2a38760e-083a-4dd0-aebc-78b570bfd3c7/s\n' +
         'cript/tasks';
-    return Promise.all([fetch(userUrl), fetch(taskUrl)]);
+    return new Promise((resolve, reject) => {
+        Promise.all([fetch(userUrl), fetch(taskUrl)])
+            .then(res => {
+                Promise.all([res[0].json(), res[1].json()])
+                    .then(response => {
+                        resolve({
+                            userList: response[0],
+                            taskList: response[1],
+                        });
+                    });
+            });
+    });
 }
 
-function renderUsersList(users) {
+function renderUsersList(userList) {
 
-
+    const getHtml = () => {
+        console.log(userList);
+        return userList.map(user => {
+            return `
+                    <li> ${user.firstName} ${user.surname} </li>
+                `;
+        }).join('')
+    };
+    document.querySelector('.users_list').innerHTML = getHtml();
 }
 
-function renderTasksList(tasks) {
-    console.log(tasks.json())
+function renderTasksList(taskList) {
+    console.log(taskList)
+    const getHtml = () => {
+        return taskList.map(task => {
+            return `
+                    <li> ${task.subject} </li>
+                `;
+        }).join('')
+    };
+    document.querySelector('.backlog').innerHTML = getHtml()
 }
 
 function togglePreloader() {
